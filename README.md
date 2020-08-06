886a5786bfc533e7c5fc20688db7c91b6aa135f2
Abednego Mwanza <abednegomwanza@gmail.com>
2020-08-06 18:27:07 +0200

    Initialize development

---
+++ README.md
@@ -0,0 +1,7 @@
+# WEB TEMPLATE
+
+### DEVELOPMENT GUIDES
+### CSS naming conventions
+We will be using BEM css naming convention : [BEM](http://getbem.com/introduction/)
+Learn more : [Maintainable css with BEM](https://www.integralist.co.uk/posts/bem/#4)
+
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/global.styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>Web Template</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
