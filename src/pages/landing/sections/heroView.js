6d2cb4c4c2cc4627d92bb8a169af14a2e3d094ad
Mikemnjovu <mikemnjovu@gmail.com>
2020-08-27 17:50:13 +0200

    add Desktop branch and items

--- src/pages/landing/sections/heroView.js
+++ src/pages/landing/sections/heroView.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { LANDING_IMAGE_DATA } from "../../../data/constants";
+import HeroImage from "../../../components/Img/HeroImage";
 import "../styles/heroView.css";
 import BaseButton from "../../../components/button/baseButton";
+import ImageComp from "../../../components/Img/imageComp";
 const HeroView = () => {
   const { title, content } = LANDING_IMAGE_DATA;
 
   return (
     <div className="hero">
-      <div>
-        <h2 className="hero__title">{title}</h2>
-      </div>
-      <div className="hero__content">
-        <p>{content}</p>
-      </div>
-      <div>
-        <BaseButton btnData={LANDING_IMAGE_DATA.button} />
+      <div className="hero__text">
+        <div>
+          <h2 className="hero__title">{title}</h2>
+        </div>
+        <div className="hero__content">
+          <p>{content}</p>
+        </div>
+        <div>
+          <BaseButton btnData={LANDING_IMAGE_DATA.button} />
+        </div>
+        <div>
+          <ImageComp imgSrc={<HeroImage />} />
+        </div>
       </div>
     </div>
   );
