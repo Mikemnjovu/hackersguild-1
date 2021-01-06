e1ac608616f7aa92a4aaf86179217adcbb0c636d
mikemnjovu <mikemnjovu@gmail.com>
2021-01-06 15:45:31 +0200

    changes

--- src/components/what_we_do_item/what-we-do-item.js
+++ src/components/what_we_do_item/what-we-do-item.js
@@ -1,25 +1,32 @@
-import React from "react";
-import BaseButton from "../button/base-button";
-import ImageComp from "../Img/image-componet";
+import React from 'react';
+import BaseButton from '../button/base-button';
+import ImageComp from '../Img/image-componet';
 
-const WhatWeDoItem = ({ props: { title, discription, button }, img }) => {
+const WhatWeDoItem = ({
+  props: { title, discription, button },
+  img,
+}) => {
   return (
     <div>
-      <div className="what-we-do__">
-        <div className="what-we-do__img">
+      <div className=' what-we-do_item'>
+        <div className='what-we-do__img'>
           <ImageComp
             imgSrc={img}
-            className={"what-we-do__hackerthons what-we-do__img"}
+            className={
+              'what-we-do__hackerthons what-we-do__img'
+            }
           />
         </div>
-        <div className="what-we-do__title">
+        <div className='what-we-do__title'>
           <h3>{title}</h3>
         </div>
-        <div className="what-we-do__discription">
+        <div className='what-we-do__discription'>
           <p>{discription}</p>
         </div>
-        <div className="what-we-do__button">
-          <BaseButton btnData={button} />
+        <div className='what-we-do__button'>
+          <BaseButton
+            btnData={button}
+          />
         </div>
       </div>
     </div>
--- src/pages/landing/sections/what-we-do.js
+++ src/pages/landing/sections/what-we-do.js
@@ -1,28 +1,40 @@
-import React from "react";
-import "../styles/what-we-do.css";
-import { SECTIONS, WHAT_WE_DO } from "../../../data/constants";
-import WhatWeDoItem from "../../../components/what_we_do_item/what-we-do-item";
-import Hackerthon from "../../../assets/images/pngs/hackerthons.png";
-import Bootcamps from "../../../assets/images/pngs/bootcamps.png";
-import SchoolClubs from "../../../assets/images/pngs/school-club.png";
-import HackerthonsBtn from "../../../components/button/hackerthon-button";
-import BoootCampsBtn from "../../../components/button/bootcamp-button";
-import SchoolClubsBtn from "../../../components/button/schoolclubs-button";
+import React from 'react';
+import '../styles/what-we-do.css';
+import {
+  SECTIONS,
+  WHAT_WE_DO,
+} from '../../../data/constants';
+import WhatWeDoItem from '../../../components/what_we_do_item/what-we-do-item';
+import Hackerthon from '../../../assets/images/pngs/hackerthons.png';
+import Bootcamps from '../../../assets/images/pngs/bootcamps.png';
+import SchoolClubs from '../../../assets/images/pngs/school-club.png';
+import HackerthonsBtn from '../../../components/button/hackerthon-button';
+import BoootCampsBtn from '../../../components/button/bootcamp-button';
+import SchoolClubsBtn from '../../../components/button/schoolclubs-button';
 
 const WhatWeDo = () => {
-  const { hackerthons, bootCamps, schoolClubs } = WHAT_WE_DO;
+  const {
+    hackerthons,
+    bootCamps,
+    schoolClubs,
+  } = WHAT_WE_DO;
   const whatWeDo = SECTIONS.firsSection;
 
   return (
-    <div className="what-we-do" id="what-we-do">
-      <div className="what-we-do__section__title">
+    <div
+      className='what-we-do'
+      id='what-we-do'
+    >
+      <div className='what-we-do__section__title'>
         <h1>{whatWeDo}</h1>
       </div>
-      <div className="what-we-do__item">
+      <div className='what-we-do__items'>
         <WhatWeDoItem
           props={hackerthons}
           img={Hackerthon}
-          buttontype={<HackerthonsBtn />}
+          buttontype={
+            <HackerthonsBtn />
+          }
         />
         <WhatWeDoItem
           props={bootCamps}
@@ -32,7 +44,9 @@ const WhatWeDo = () => {
         <WhatWeDoItem
           props={schoolClubs}
           img={SchoolClubs}
-          buttontype={<SchoolClubsBtn />}
+          buttontype={
+            <SchoolClubsBtn />
+          }
         />
       </div>
     </div>
