3ea55106e0c4df9b9514bc08c20a17e03666e6a5
Abednego Mwanza <abednegomwanza@gmail.com>
2021-01-29 17:33:55 +0200

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "../styles/global-styles.css";
 import Footer from "../components/footer/footer";
-import FooterMobile from "../components/footer/footer-mobile";
 import LandingPage from "./landing/landing-page";
 import HackerthonsPage from "./learn_more/hackerthons";
 import BootcampsPage from "./learn_more/bootcamps";
 import SchoolClubsPage from "./learn_more/school-clubs";
-import { NavContext, NavProvider } from "../data/state/nav-state";
 import { NavSetState } from "../data/state/nav-setState";
 import Nav from "../components/nav/nav";
 
 function App() {
-  const [navShowState, setNavShowState] = useContext(NavContext);
   NavSetState(true);
   return (
     <>
       <div className="App">
-        <Router>
+        <Router basename={"/"}>
           <Nav />
           <Switch>
             <Route exact path="/" component={LandingPage} />
