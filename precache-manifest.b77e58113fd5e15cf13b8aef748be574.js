cce24e64c75f7cb7cb708201b1f79ad151b05195
Abednego Mwanza <abednegomwanza@gmail.com>
2021-01-29 11:24:57 +0200

    Updates

---
+++ precache-manifest.b77e58113fd5e15cf13b8aef748be574.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84529a9cec213e73bf21f98c7864f7eb",
+    "url": "/hackersguild/index.html"
+  },
+  {
+    "revision": "219003b4bab30d2bfe44",
+    "url": "/hackersguild/static/css/main.d2732add.chunk.css"
+  },
+  {
+    "revision": "dce7e697e180b425cec9",
+    "url": "/hackersguild/static/js/2.0794ff53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/hackersguild/static/js/2.0794ff53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219003b4bab30d2bfe44",
+    "url": "/hackersguild/static/js/main.e6e15d15.chunk.js"
+  },
+  {
+    "revision": "7da9e2f8b3b27020e595",
+    "url": "/hackersguild/static/js/runtime-main.165fe713.js"
+  },
+  {
+    "revision": "1d20333d492f9e2ad2af4423d61bf6bb",
+    "url": "/hackersguild/static/media/Get-in-touch.1d20333d.png"
+  },
+  {
+    "revision": "0766a44c2de24e01ecf1405f694b808b",
+    "url": "/hackersguild/static/media/HeroImage.0766a44c.png"
+  },
+  {
+    "revision": "e055d57be0a5351b34a3c1601f501f4a",
+    "url": "/hackersguild/static/media/bootcamps.e055d57b.png"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/hackersguild/static/media/github-logo.cefc2023.png"
+  },
+  {
+    "revision": "1dd472fc75348162cc4755ea0567d7fb",
+    "url": "/hackersguild/static/media/hackerthons.1dd472fc.png"
+  },
+  {
+    "revision": "641911869f99703a77adf12fb3892742",
+    "url": "/hackersguild/static/media/open-energy-labs.64191186.png"
+  },
+  {
+    "revision": "8c5f34e41333b68ddbb35941f65546c2",
+    "url": "/hackersguild/static/media/school-club.8c5f34e4.png"
+  }
+]);
